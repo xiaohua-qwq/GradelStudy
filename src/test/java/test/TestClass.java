8722f31372b9e7dc05ac2038878e835414dca618
xiaohua <zuwias7@gmail.com>
2024-04-17 21:09:47 +0800

    提交

---
+++ src/test/java/test/TestClass.java
@@ -0,0 +1,11 @@
+package test;
+
+
+import org.junit.jupiter.api.Test;
+
+public class TestClass {
+    @Test
+    public void testFunction() {
+        System.out.println("Test SuccessFully");
+    }
+}
