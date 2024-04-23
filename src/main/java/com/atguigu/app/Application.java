600fb37206611553fa67f8309f07f568e82e8db3
xiaohua <zuwias7@gmail.com>
2024-04-23 20:53:44 +0800

    ..

--- src/main/java/com/atguigu/app/Application.java
+++ src/main/java/com/atguigu/app/Application.java
@@ -2,6 +2,7 @@ package com.atguigu.app;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String name = "张三";
+        System.out.println("Hello " + name);
     }
 }
